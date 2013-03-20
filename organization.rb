cff56cff1a4ceeb8e5634f0ccd50bda4854cd1c7
Tyler Wymer <twymer@gmail.com>
2013-03-19 22:08:57 -0400

    Method descriptions added

--- organization.rb
+++ organization.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Organization
   @@possible_permission_types = [:admin, :user, :disabled]
 
@@ -12,12 +10,17 @@ class Organization
     @@possible_permission_types
   end
 
+  # Add user with a given permission value to this organization
   def add_user(user, permission = :user)
     if possible_permission_types.include? permission
       @users.merge!({ user.id => permission })
     end
   end
 
+  # Check this organization for requested user. If not present here, attempt
+  # to find an inherited permission value. The permission in the nearest
+  # organization (recursing through parents) will be used. If user is not
+  # found, return disabled
   def user_permission(user)
     if @users.has_key? user.id
       @users[user.id]
